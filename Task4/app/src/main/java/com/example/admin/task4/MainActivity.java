bbb7ae88c49a87e2ecf69c5753f1e3e44c3698a0
Arseny Skurko <ars.skurko@yandex.ru>
2014-08-07 00:55:46 +0700

    everything is done

--- Task4/app/src/main/java/com/example/admin/task4/MainActivity.java
+++ Task4/app/src/main/java/com/example/admin/task4/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,30 +16,39 @@ import com.example.admin.task4.fragments.TopFragment;
 
 public class MainActivity extends Activity {
 
+    TopFragment topLeftFragment;
+    BottomLeftFragment bottomLeftFragment;
+    BottomRightFragment bottomRightFragment;
+
     private boolean isClicked = false;
+    private static final String KEY_IS_CLICKED = "IS_CLICKED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TopFragment topLeftFragment;
-        final BottomLeftFragment bottomLeftFragment;
-        final BottomRightFragment bottomRightFragment;
+        if (savedInstanceState != null) {
+            isClicked = savedInstanceState.getBoolean(KEY_IS_CLICKED);
+        }
 
         topLeftFragment = new TopFragment();
-        getFragmentManager().beginTransaction().replace(R.id.top_left_fragment, topLeftFragment, "TOP_LEFT_FRAGMENT").commit();
+        getFragmentManager().beginTransaction().replace(R.id.top_left_fragment, topLeftFragment).commit();
         bottomLeftFragment = new BottomLeftFragment();
-        getFragmentManager().beginTransaction().replace(R.id.bottom_left_fragment, bottomLeftFragment, "BOTTOM_LEFT_FRAGMENT").commit();
+        getFragmentManager().beginTransaction().replace(R.id.bottom_left_fragment, bottomLeftFragment).commit();
         bottomRightFragment = new BottomRightFragment();
-        getFragmentManager().beginTransaction().replace(R.id.bottom_right_fragment, bottomRightFragment, "BOTTOM_RIGHT_FRAGMENT").commit();
+        getFragmentManager().beginTransaction().replace(R.id.bottom_right_fragment, bottomRightFragment).commit();
+
+        if (isClicked) {
+            swapFragments();
+        }
 
         findViewById(R.id.top_left_fragment).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isClicked) {
                     isClicked = true;
-                    //swap fragments or whatever
+                    swapFragments();
                 }
             }
         });
@@ -50,5 +60,18 @@ public class MainActivity extends Activity {
                 dialog.show(getFragmentManager(), "DIALOG");
             }
         });
+    }
+
+    private void swapFragments() {
+        bottomLeftFragment = new BottomLeftFragment();
+        bottomRightFragment = new BottomRightFragment();
+        getFragmentManager().beginTransaction().replace(R.id.bottom_right_fragment, bottomLeftFragment).commit();
+        getFragmentManager().beginTransaction().replace(R.id.bottom_left_fragment, bottomRightFragment).commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_IS_CLICKED, isClicked);
     }
 }
